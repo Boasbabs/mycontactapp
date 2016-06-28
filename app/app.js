02b6774da5f575cc6a5227b44926e645ee84cbc1
Boasbabs <babalolasimeon@gmail.com>
2016-06-28 23:55:02 +0100

    set up contact.html

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myContacts', [
-  'ngRoute'
+  'ngRoute',
+  "firebase",
+  "myContacts.contacts"
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myContacts.contacts', ['ngRoute', "firebase"])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('ContactsCtrl', ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+	console.log($scope);
 }]);
